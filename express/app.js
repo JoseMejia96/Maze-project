644b93f18d0e03d4c6101ddd5bf91eb32d68a131
Pipster96 <josefonseca1996@gmail.com>
2016-08-13 19:49:22 -0600

    Format

--- express/app.js
+++ express/app.js
@@ -28,7 +28,7 @@ app.get('/', routes.index);
 app.get('/users', users.list);
 
 /// catch 404 and forwarding to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
@@ -39,7 +39,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
+    app.use(function (err, req, res, next) {
         res.render('error', {
             message: err.message,
             error: err
@@ -49,91 +49,91 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     res.render('error', {
         message: err.message,
         error: {}
     });
 });
 
-app.post('/', function(req, res){
+app.post('/', function (req, res) {
     res.send(DrawMaze());
 });
 
 module.exports = app;
 
 //-----------------------maze------------------
-function maze(x,y) {
-	var n=x*y-1;
-	if (n<0) {alert("illegal maze dimensions");return;}
-	var horiz =[]; for (var j= 0; j<x+1; j++) horiz[j]= [],
-	    verti =[]; for (var j= 0; j<x+1; j++) verti[j]= [],
-	    here = [Math.floor(Math.random()*x), Math.floor(Math.random()*y)],
-	    path = [here],
-	    unvisited = [];
-	for (var j = 0; j<x+2; j++) {
+function maze(x, y) {
+	var n = x * y - 1;
+	if (n < 0) { alert("illegal maze dimensions"); return; }
+	var horiz = []; for (var j = 0; j < x + 1; j++) horiz[j] = [],
+		verti = []; for (var j = 0; j < x + 1; j++) verti[j] = [],
+			here = [Math.floor(Math.random() * x), Math.floor(Math.random() * y)],
+			path = [here],
+			unvisited = [];
+	for (var j = 0; j < x + 2; j++) {
 		unvisited[j] = [];
-		for (var k= 0; k<y+1; k++)
-			unvisited[j].push(j>0 && j<x+1 && k>0 && (j != here[0]+1 || k != here[1]+1));
+		for (var k = 0; k < y + 1; k++)
+			unvisited[j].push(j > 0 && j < x + 1 && k > 0 && (j != here[0] + 1 || k != here[1] + 1));
 	}
-	while (0<n) {
-		var potential = [[here[0]+1, here[1]], [here[0],here[1]+1],
-		    [here[0]-1, here[1]], [here[0],here[1]-1]];
+	while (0 < n) {
+		var potential = [[here[0] + 1, here[1]], [here[0], here[1] + 1],
+			[here[0] - 1, here[1]], [here[0], here[1] - 1]];
 		var neighbors = [];
 		for (var j = 0; j < 4; j++)
-			if (unvisited[potential[j][0]+1][potential[j][1]+1])
+			if (unvisited[potential[j][0] + 1][potential[j][1] + 1])
 				neighbors.push(potential[j]);
 		if (neighbors.length) {
-			n = n-1;
-			next= neighbors[Math.floor(Math.random()*neighbors.length)];
-			unvisited[next[0]+1][next[1]+1]= false;
+			n = n - 1;
+			next = neighbors[Math.floor(Math.random() * neighbors.length)];
+			unvisited[next[0] + 1][next[1] + 1] = false;
 			if (next[0] == here[0])
-				horiz[next[0]][(next[1]+here[1]-1)/2]= true;
+				horiz[next[0]][(next[1] + here[1] - 1) / 2] = true;
 			else
-				verti[(next[0]+here[0]-1)/2][next[1]]= true;
+				verti[(next[0] + here[0] - 1) / 2][next[1]] = true;
 			path.push(here = next);
 		} else
 			here = path.pop();
 	}
-	return {x: x, y: y, horiz: horiz, verti: verti};
+	return { x: x, y: y, horiz: horiz, verti: verti };
 }
 
 function GenerateMaze(m) {
-	var outstring =[];
-	for (var j= 0; j<m.x*2+1; j++) {
+	var outstring = [];
+	for (var j = 0; j < m.x * 2 + 1; j++) {
 		var line = [];
-		if (0 == j%2)
-			for (var k=0; k<m.y*2+1; k++)
-				if (0 == k%2){
+		if (0 == j % 2)
+			for (var k = 0; k < m.y * 2 + 1; k++)
+				if (0 == k % 2) {
 
-          line[k]= 1;
-        }
+					line[k] = 1;
+				}
 				else
-					if (j>0 && m.verti[j/2-1][Math.floor(k/2)])
-						line[k]= 0;
+					if (j > 0 && m.verti[j / 2 - 1][Math.floor(k / 2)])
+						line[k] = 0;
 					else
-						line[k]= 1;
+						line[k] = 1;
 		else
-			for (var k=0; k<m.y*2+1; k++)
-				if (0 == k%2)
-					if (k>0 && m.horiz[(j-1)/2][k/2-1])
-						line[k]= 0;
+			for (var k = 0; k < m.y * 2 + 1; k++)
+				if (0 == k % 2)
+					if (k > 0 && m.horiz[(j - 1) / 2][k / 2 - 1])
+						line[k] = 0;
 					else
-						line[k]= 1;
+						line[k] = 1;
 				else
-					line[k]= 0;
-		if (0 == j) line[1]= 0;
-		if (m.x*2-1 == j) line[2*m.y]= -1;
+					line[k] = 0;
+		if (0 == j) line[1] = 0;
+		if (m.x * 2 - 1 == j) line[2 * m.y] = -1;
 		outstring.push(saveLine(line));
 	}
 	return outstring;
 }
 
-function saveLine(line){
-	return {linea :line};
+function saveLine(line) {
+	return { linea: line };
 }
 
-function DrawMaze(){
+function DrawMaze() {
 	console.log("Dibujando!!")
-		return GenerateMaze(maze(30,40));
+	return GenerateMaze(maze(30, 40));
 }
--- express/public/javascripts/client-server.js
+++ express/public/javascripts/client-server.js
@@ -1,32 +1,32 @@
 var canvas;
 var maze = [];
-window.onload = function(){
-	Laberinto();
-	canvas = $('#Maze');
-	document.onkeydown = press;
+window.onload = function () {
+    Laberinto();
+    canvas = $('#Maze');
+    document.onkeydown = press;
 }
 
-function Laberinto(){
-var data = {};
-	data.title = "title";
-	data.message = "message";
+function Laberinto() {
+    var data = {};
+    data.title = "title";
+    data.message = "message";
 
-		$.ajax({
-			type: 'POST',
-	        contentType: 'application/json',
-	        url: 'http://localhost:3000',
-	        success: function(data) {
-	        	maze = jsonMaze(data);
-						draw();
-	          console.log('success');
-	        }
-	    });
+    $.ajax({
+        type: 'POST',
+        contentType: 'application/json',
+        url: 'http://localhost:3000',
+        success: function (data) {
+            maze = jsonMaze(data);
+            draw();
+            console.log('success');
+        }
+    });
 }
 
-function jsonMaze(data){
-	var mazeTemp= [];
-	data.forEach(x=>mazeTemp.push(x.linea));
-	return mazeTemp;
+function jsonMaze(data) {
+    var mazeTemp = [];
+    data.forEach(x => mazeTemp.push(x.linea));
+    return mazeTemp;
 }
 
 var player = {
@@ -35,52 +35,52 @@ var player = {
 };
 
 //Draw the game maze
-function draw(){
-	console.log("entra");
+function draw() {
+    console.log("entra");
     var width = canvas.width();
-    var blockSize = width/((maze.length)+20);
+    var blockSize = width / ((maze.length) + 20);
     var ctx = canvas[0].getContext('2d');
     ctx.setTransform(1, 0, 0, 1, 0, 0);
     ctx.clearRect(0, 0, width, width);
-    ctx.fillStyle="black";
+    ctx.fillStyle = "black";
     //Loop through the maze array drawing the walls and the goal
-    for(var y = 0; y < maze.length; y++){
-        for(var x = 0; x < maze[y].length; x++){
+    for (var y = 0; y < maze.length; y++) {
+        for (var x = 0; x < maze[y].length; x++) {
             //Draw a wall
-						(maze[y][x]===1)?(ctx.fillRect(x*blockSize, y*blockSize, blockSize, blockSize)):(
-							(maze[y][x]===-1)?(
-						ctx.beginPath(),
-						ctx.lineWidth=5,
-						ctx.strokeStyle="gold",
-						ctx.moveTo(x*blockSize,y*blockSize),
-						ctx.lineTo((x+1)*blockSize,(y+1)*blockSize),
-						ctx.moveTo(x*blockSize,(y+1)*blockSize),
-						ctx.lineTo((x+1)*blockSize, y*blockSize),
-						ctx.stroke()):0);
+            (maze[y][x] === 1) ? (ctx.fillRect(x * blockSize, y * blockSize, blockSize, blockSize)) : (
+                (maze[y][x] === -1) ? (
+                    ctx.beginPath(),
+                    ctx.lineWidth = 5,
+                    ctx.strokeStyle = "gold",
+                    ctx.moveTo(x * blockSize, y * blockSize),
+                    ctx.lineTo((x + 1) * blockSize, (y + 1) * blockSize),
+                    ctx.moveTo(x * blockSize, (y + 1) * blockSize),
+                    ctx.lineTo((x + 1) * blockSize, y * blockSize),
+                    ctx.stroke()) : 0);
         }
     }
     //Draw the player
     ctx.beginPath();
-    var half = blockSize/2;
+    var half = blockSize / 2;
     ctx.fillStyle = "red";
-    ctx.arc(player.x*blockSize+half, player.y*blockSize+half, half, 0, 2*Math.PI);
+    ctx.arc(player.x * blockSize + half, player.y * blockSize + half, half, 0, 2 * Math.PI);
     ctx.fill();
 }
 
 //Check to see if the new space is inside the maze and not a wall
-function canMove(x, y){
-    return (y>=0) && (y<maze.length) && (x >= 0) && (x < maze[y].length) && (maze[y][x] != 1);
+function canMove(x, y) {
+    return (y >= 0) && (y < maze.length) && (x >= 0) && (x < maze[y].length) && (maze[y][x] != 1);
 }
 
 
-function press(e){
-    if((e.which == 38) && canMove(player.x, player.y-1))//Up arrow
+function press(e) {
+    if ((e.which == 38) && canMove(player.x, player.y - 1))//Up arrow
         player.y--;
-    else if((e.which == 40) && canMove(player.x, player.y+1)) // down arrow
+    else if ((e.which == 40) && canMove(player.x, player.y + 1)) // down arrow
         player.y++;
-    else if((e.which == 37) && canMove(player.x-1, player.y))
+    else if ((e.which == 37) && canMove(player.x - 1, player.y))
         player.x--;
-    else if((e.which == 39) && canMove(player.x+1, player.y))
+    else if ((e.which == 39) && canMove(player.x + 1, player.y))
         player.x++;
     draw();
     e.preventDefault();
