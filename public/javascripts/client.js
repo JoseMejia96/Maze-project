03f6b9dc04a2528a3ed5f9d891b0b5dd1e07c782
chuzmurillo <chuzmurillo@gmail.com>
2016-09-04 19:45:54 -0600

    cambios V#2

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -217,7 +217,7 @@ function press(e) {
     draw();
     e.preventDefault();
 }
-//--------------------Backtracking miedo find path to win-----------------------------------
+//--------------------Backtracking miedo -----------------------------------
 function searchMaze(y, x) {
     var sol;
     var ly = maze.length - 1;
@@ -306,7 +306,7 @@ function fillUnvisited(un, x, y) {
         if (j < x + 2) {
             flag ? un[j] = [] : un;
             (k < y + 1) ? (
-                un[j].push(j > 0 && j < x + 1 && k > 0 && (j != here[0] + 1 || k != here[1] + 1)),
+                un[j].push(j > 0 && j < x + 1 && k > 0 && (j != act[0] + 1 || k != act[1] + 1)),
                 doIt(un, j, k + 1, x, y, false)
             ) : (doIt(un, j + 1, 0, x, y, true));
         }
@@ -322,22 +322,22 @@ function fillPotential(un, po, j, pos, n) {
     }
 }
 
-function lastOne(n, unvisited, horiz, verti, path, here) {
+function lastOne(n, solas, horiz, verti, fin, act) {
     if (0 < n) {
-        var potential = [[here[0] + 1, here[1]], [here[0], here[1] + 1], [here[0] - 1, here[1]], [here[0], here[1] - 1]];
-        var neighbors = [];
-        fillPotential(unvisited, potential, 0, 4, neighbors);
-        (neighbors.length) ? (
-            next = neighbors[Math.floor(Math.random() * neighbors.length)],
-            unvisited[next[0] + 1][next[1] + 1] = false,
-            (next[0] == here[0]) ?
-                (horiz[next[0]][(next[1] + here[1] - 1) / 2] = true) :
-                (verti[(next[0] + here[0] - 1) / 2][next[1]] = true),
-            path.push(here = next),
-            lastOne(n - 1, unvisited, horiz, verti, path, here)
+        var po = [[act[0] + 1, act[1]], [act[0], act[1] + 1], [act[0] - 1, act[1]], [act[0], act[1] - 1]];
+        var ve = [];
+        fillPotential(solas, po, 0, 4, ve);
+        (ve.length) ? (
+            sig = ve[Math.floor(Math.random() * ve.length)],
+            solas[sig[0] + 1][sig[1] + 1] = false,
+            (sig[0] == act[0]) ?
+                (horiz[sig[0]][(sig[1] + act[1] - 1) / 2] = true) :
+                (verti[(sig[0] + act[0] - 1) / 2][sig[1]] = true),
+            fin.push(act = sig),
+            lastOne(n - 1, solas, horiz, verti, fin, act)
         ) : (
-                here = path.pop(),
-                lastOne(n, unvisited, horiz, verti, path, here)
+                act = fin.pop(),
+                lastOne(n, solas, horiz, verti, fin, act)
             );
     }
 
@@ -349,38 +349,38 @@ function mazze(x, y) {
     var horiz = [], verti = [];
     let fillArray1 = j => (horiz[j] = []);
     let fillArray2 = j => (verti[j] = [],
-        here = [Math.floor(Math.random() * x), Math.floor(Math.random() * y)],
-        path = [here],
-        unvisited = []
+        act = [Math.floor(Math.random() * x), Math.floor(Math.random() * y)],
+        fin = [act],
+        solas = []
     )
     horiz = RangeArray(x + 1, z => fillArray1(0, [], []));
     verti = RangeArray(x + 1, q => fillArray2(0));
-    fillUnvisited(unvisited, x, y);
-    lastOne(x * y - 1, unvisited, horiz, verti, path, here);
+    fillUnvisited(solas, x, y);
+    lastOne(x * y - 1, solas, horiz, verti, fin, act);
     return { x: x, y: y, horiz: horiz, verti: verti };
 }
 
 function GenerateMaze(m) {
-    function mmaze(j, k, outstring, line, m) {
+    function mmaze(j, k, salida, li, m) {
         if (j < m.x * 2 + 1) {
             if (k < m.y * 2 + 1) {
                 (j % 2 == 0) ? (
-                    (0 == k % 2) ? line[k] = 1 : ((j > 0 && m.verti[j / 2 - 1][Math.floor(k / 2)]) ? line[k] = 0 : line[k] = 1)
+                    (0 == k % 2) ? li[k] = 1 : ((j > 0 && m.verti[j / 2 - 1][Math.floor(k / 2)]) ? li[k] = 0 : li[k] = 1)
                 ) : (
-                        (0 == k % 2) ? ((k > 0 && m.horiz[(j - 1) / 2][k / 2 - 1]) ? line[k] = 0 : line[k] = 1) : line[k] = 0)
-                mmaze(j, k + 1, outstring, line, m);
+                        (0 == k % 2) ? ((k > 0 && m.horiz[(j - 1) / 2][k / 2 - 1]) ? li[k] = 0 : li[k] = 1) : li[k] = 0)
+                mmaze(j, k + 1, salida, li, m);
             } else {
-                (0 == j) ? line[1] = 0 : line;
-                (m.x * 2 - 1 == j) ? line[2 * m.y] = -1 : line;
-                outstring.push(saveLine(line));
-                mmaze(j + 1, 0, outstring, [], m);
+                (0 == j) ? li[1] = 0 : li;
+                (m.x * 2 - 1 == j) ? li[2 * m.y] = -1 : li;
+                salida.push(saveLine(li));
+                mmaze(j + 1, 0, salida, [], m);
             }
         }
-        return outstring;
+        return salida;
     }
     return mmaze(0, 0, [], [], m);
 }
 
-function saveLine(line) {
-    return { linea: line };
+function saveLine(li) {
+    return { linea: li };
 }
